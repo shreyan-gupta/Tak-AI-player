3083dc3a6ae44b8249887ba03bf0f483aa7fc5f5
Shreyan <shreyan.gupta96@gmail.com>
2016-09-24 17:31:55 +0530

    small changes

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,2 +1,6 @@
 #include "Header.h"
 
+Board::Board(int size){
+	GameBoard = vector< vector<Position> >(size, vector<Position>(size));
+}
+
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,1 +1,8 @@
 #include "Header.h"
+
+
+Player::Player(bool player_type, int pieces, int caps){
+	MinMax = player_type;
+	StonesLeft = pieces;
+	CapsLeft = caps;
+}
