ce5549e330e7c9ebf0cb76a3e12fe8596f1f1602
aditi741997 <aditi741997@gmail.com>
2016-09-24 17:18:42 +0530

    Yo!

--- src/Header.h
+++ src/Header.h
@@ -5,5 +5,38 @@
 #include <stack>
 #include <vector>
 
+using namespace std;
+
+typedef std::pair<Stone,Player_type> Piece;
+
+struct Position
+{
+	std::stack<Piece> Stack;
+	Player_Type getPlayer()
+	{
+		if (Stack.size() == 0)
+			return none;
+		else
+			return Stack.top().second;
+	}
+};
+
+struct Move
+{
+	
+};
+
+
+class Board
+{
+	public:
+		vector<vector<Position> > GameBoard;
+
+		Board();
+		~Board();
+		eval();
+		makemove();		// inputs yet to define
+};
+
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
 
-using namespace std;
+#include "Header.h"
 
 int main(int argc, char const *argv[])
 {
